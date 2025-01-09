fbafcac55c9b4bc33e0442f6f1f0cc6b8c066816
Eduardo Argenti <eduardosilvargenti@gmail.com>
2025-01-09 20:34:47 -0300

    Adding products to existing plans

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,16 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from repositories.plan_repository import create_plan_with_products, get_all_plans, get_plan_by_id
+from repositories.plan_repository import create_plan_with_products, get_all_plans, get_plan_by_id, add_products_to_plans
 from schemas.Plan import PlanCreate, PlanResponse
+from schemas.Product import ProductCreate
 from typing import List
 
 router = APIRouter()
-
-@router.post("/plans/", response_model=PlanResponse)
-def create_plan_route(plan: PlanCreate, db: Session = Depends(get_db)):
-    db_plan = create_plan_with_products(db, value=plan.value, products=plan.products)
-    return db_plan
 
 @router.get("/plans/", response_model=List[PlanResponse])
 def read_plans_route(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
@@ -23,3 +19,13 @@ def read_plan_route(plan_id: int, db: Session = Depends(get_db)):
     if plan is None:
         raise HTTPException(status_code=404, detail=f"Plan not found")
     return plan
+
+@router.post("/plans/", response_model=PlanResponse)
+def create_plan_route(plan: PlanCreate, db: Session = Depends(get_db)):
+    db_plan = create_plan_with_products(db, value=plan.value, products=plan.products)
+    return db_plan
+
+@router.patch("/plans/{plan_id}/products", response_model=PlanResponse)
+def add_products_route(plan_id: int, products: List[ProductCreate], db: Session = Depends(get_db)):
+    db_plan = add_products_to_plans(db, id=plan_id, products=products, mode="add")
+    return db_plan
--- app/repositories/plan_repository.py
+++ app/repositories/plan_repository.py
@@ -3,6 +3,8 @@ from models.Plan import Plan
 from models.Product import Product
 from typing import List, Optional
 from schemas.Product import ProductCreate
+from fastapi import HTTPException
+from datetime import datetime
 
 def create_plan(db: Session, value: float) -> Plan:
     db_plan = Plan(value=value)
@@ -10,6 +12,12 @@ def create_plan(db: Session, value: float) -> Plan:
     db.commit()
     db.refresh(db_plan)
     return db_plan
+
+def get_all_plans(db: Session, skip: int = 0, limit: int = 10) -> List[Plan]:
+    return db.query(Plan).offset(skip).limit(limit).all()
+
+def get_plan_by_id(db: Session, plan_id: int) -> Optional[Plan]:
+    return db.query(Plan).filter(Plan.id == plan_id).first()
 
 def create_plan_with_products(db: Session, value: float, products: List[ProductCreate]) -> Plan:
     db_plan = Plan(value=value)
@@ -31,8 +39,29 @@ def create_plan_with_products(db: Session, value: float, products: List[ProductC
 
     return db_plan
 
-def get_all_plans(db: Session, skip: int = 0, limit: int = 10) -> List[Plan]:
-    return db.query(Plan).offset(skip).limit(limit).all()
+def add_products_to_plans(db: Session, id: int, products: List[ProductCreate], mode: str) -> Plan:
+    db_plan = db.query(Plan).filter(Plan.id == id).first()
 
-def get_plan_by_id(db: Session, plan_id: int) -> Optional[Plan]:
-    return db.query(Plan).filter(Plan.id == plan_id).first()
+    if not db_plan:
+        raise HTTPException(status_code=404, detail=f"Plan with ID {id} not found")
+
+    if mode == "add":
+        for product in products:
+            db_product = Product(
+                name=product.name,
+                desc=product.desc,
+                plan_id=db_plan.id
+            )
+            db.add(db_product)
+    elif mode == "delete":
+        pass
+    else:
+        raise HTTPException(status_code=400, detail="Invalid mode. Use 'add' or 'delete'.")
+
+    db_plan.updated_at = datetime.utcnow()
+    db.add(db_plan)
+
+    db.commit()
+    db.refresh(db_plan)
+
+    return db_plan
