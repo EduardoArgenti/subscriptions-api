841f525f8c5d17fd0705fd3b460780178ba9a69d
Eduardo Argenti <eduardosilvargenti@gmail.com>
2025-01-09 21:52:53 -0300

    Validate optional params for Products

--- app/schemas/Product.py
+++ app/schemas/Product.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class ProductCreate(BaseModel):
     name: str
-    desc: str
+    desc: Optional[str] = None
 
 class ProductResponse(BaseModel):
     id: int
     name: str
-    desc: str
+    desc: Optional[str]
     active: bool
     created_at: datetime
     updated_at: datetime
